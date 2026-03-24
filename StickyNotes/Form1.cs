162e8eeb2287424d96a903a9fe64f839de9027da
Student <Student@C105_25>
2026-03-24 09:40:14 +0100

    Update and Delete

--- StickyNotes/Form1.cs
+++ StickyNotes/Form1.cs
@@ -38,7 +38,51 @@ namespace StickyNotes
             foreach (StickyNote stickyNote in db.StickyNotes)
             {
                 RichTextBox rtb = new RichTextBox();
+                rtb.Tag = stickyNote;
+                rtb.Text = stickyNote.NoteText;
+
+                rtb.TextChanged += Rtb_TextChanged;
+                rtb.MouseDown += Rtb_MouseDown;
                 flowLayoutPanelList.Controls.Add(rtb);
+            }
+        }
+
+        private void Rtb_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (sender is RichTextBox)
+                {
+                    RichTextBox rtb = sender as RichTextBox;
+                    if (rtb.Tag is StickyNote)
+                    {
+                        StickyNote myStickyNote = rtb.Tag as StickyNote;
+                        if (MessageBox.Show($"Are you sure you want to delete the sticyNote '{myStickyNote.NoteText}'?",
+                                            "Confirm Deletion",
+                                            MessageBoxButtons.YesNo,
+                                            MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            db.StickyNotes.DeleteOnSubmit(myStickyNote);
+                            db.SubmitChanges();
+                            showStickyNotes();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Rtb_TextChanged(object sender, EventArgs e)
+        {
+            if(sender is RichTextBox)
+            {
+                RichTextBox rtb = sender as RichTextBox;
+                if(rtb.Tag is StickyNote)
+                {
+                    StickyNote myStickyNote = rtb.Tag as StickyNote;
+                    myStickyNote.NoteText = rtb.Text;
+                    db.SubmitChanges();
+                    //showStickyNotes();
+                }
             }
         }
     }

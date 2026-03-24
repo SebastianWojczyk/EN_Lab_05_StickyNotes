549c91db26f45cdda149f16033a97feced6e8f6a
Student <Student@C105_25>
2026-03-24 09:09:06 +0100

    Add project files.

---
+++ StickyNotes/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StickyNotes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+/*
+CREATE TABLE [dbo].[StickyNote]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [NoteText] NVARCHAR(MAX) NOT NULL
+)
+*/
